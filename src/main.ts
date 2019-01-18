9b85d63105567656b4b2c931c7442b36ad554df4
Erik Onarheim <erik.onarheim@gmail.com>
2019-01-17 22:07:02 -0600

    Switch to scene

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@
 import * as ex from 'excalibur';
 import Config from './config';
 import { Resources, loader } from './resources';
-import { Ship } from './actors/Ship';
-import { HealthBar } from './actors/health-bar';
-import { Baddie } from './actors/baddie';
-import { animManager } from './actors/animation-manager';
-import { stats } from './stats';
+import { Game } from './game';
 
 const game = new ex.Engine({
     backgroundColor: ex.Color.Black
@@ -14,43 +10,9 @@ const game = new ex.Engine({
 game.backgroundColor = ex.Color.Black;
 game.setAntialiasing(false);
 
-game.add(animManager);
-
-
-const ship = new Ship(game.halfDrawWidth, 800, 80, 80);
-game.add(ship);
-
-const healthBar = new HealthBar();
-game.add(healthBar);
-
-const scoreLabel = new ex.Label("Score: " + stats.score, 20, 50);
-scoreLabel.color = ex.Color.Azure;
-scoreLabel.scale = new ex.Vector(3, 3);
-scoreLabel.on('preupdate', function(evt){
-    this.text = "Score: " + stats.score;
-});
-game.add(scoreLabel);
-
-
-const gameOverLabel = new ex.Label("Game Over", game.halfDrawWidth - 250, game.halfDrawHeight);
-gameOverLabel.color = ex.Color.Green.clone();
-gameOverLabel.scale = new ex.Vector(8,8);
-gameOverLabel.actions.blink(1000, 1000, 400).repeatForever();
-
-
-
-let baddieTimer = new ex.Timer(() => {
-    var bad = new Baddie(Math.random()*1000 + 200, -100, 80, 80);
-    game.add(bad);    
-}, Config.spawnTime, true, -1);
-
-game.addTimer(baddieTimer);
-
-game.on('preupdate', (evt: ex.PreUpdateEvent) => {
-    if (stats.gameOver) {
-        game.add(gameOverLabel);
-    }
-});
+// Setup game scene
+game.add('game', new Game());
+game.goToScene('game');
 
 // Game events to handle
 game.on('hidden', () => {
